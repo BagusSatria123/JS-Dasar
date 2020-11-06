cd54f75d1007640ab0f363c044e71909e42efb2c
BagusSatria123 <satriabagusputra123@gmail.com>
2020-11-06 22:54:32 +0700

    Done2

---
+++ app4.js
@@ -0,0 +1,54 @@
+//javascript scope global scope dan local scope (kapan suatu variabel dapat diakses dan variabel tidak dapat diakses)
+/*
+//local scope
+//1
+// function myFunction() {
+//   const name = "wegodev";
+//   console.log(name);
+// }
+
+//2
+// const name = "Anyom";//Global Scope
+// function myFunction() {
+//   const name = "wegodev";
+
+//   function insideFunc() {
+//     console.log(name);
+//   }
+//   insideFunc();
+// }
+// myFunction();
+
+//var
+var globalVar = "global VAr";
+console.log(window.globalVar);
+*/
+
+//javascript hoisting
+/*
+//1
+// x = 20; //assigning
+// y = 70; //assigning
+// console.log(y);
+// var x; //declaration
+// var y = 20; //variable initialitation
+// function myFunction() {
+//   console.log("hello hoisting");
+// }
+
+//2
+//function declaration
+// function myFunction() {
+//   console.log("hello hoisting");
+// }
+
+// //function expression atau deifinition
+// var myFunctionexp = function () {
+//   console.log("hello hoisting");
+// };
+
+// console.log(myFunction());
+// console.log(myFunctionexp);
+*/
+
+//Summary
